f1b25cbeb87e93dabdcd462a26853a63d274fd65
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-08-11 16:03:03 -0500

    pruevas

--- src/components/home/bannerHome.vue
+++ src/components/home/bannerHome.vue
@@ -1,74 +1,127 @@
 <template>
-  <v-app-bar
-      app
-      color="white"
-      flat
-      dense
-      class="bannerHome"
-  >
+  <div>
+    <v-navigation-drawer v-model="isActive" temporary app>
+      <v-list-item class="mt-2">
+        <div class="d-flex flex-column">
+          <span class="app-logo-title">
+            Christopher Gerardy
+          </span>
+          <span class="app-bar-subhead">
+            Desarrollador web
+          </span>
+        </div>
+      </v-list-item>
 
-    <img src="../../../public/logoSfn.png" height="80%"/>
-    <v-app-bar-title class="bannerHome--title Poppins-Light">
-      Instituto Pakal
-    </v-app-bar-title>
-    <v-spacer/>
+      <v-list>
+        <v-divider />
 
-    <div v-for="item in links" :key="item.title">
-      <v-btn
-          text
-          small
-          @click="scrollToSection(item.route)"
-      >
-        <span class="Poppins">{{ item.title }}</span>
-      </v-btn>
-    </div>
-  </v-app-bar>
+        <v-list-item v-for="(item, i) of menuLinks" :key="i" link @click="scrollToSection(item.route)">
+          <v-list-item-content>
+            <v-list-item-title class="app-font-size">
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar elevation="0" class="app-bar-style" color="white" dense>
+
+      <img src="../../../public/logoSfn.png" height="36px"/>
+      <div class="container app-bar-content">
+        <div class="d-flex ">
+          <span class="app-logo-title">
+            Christopher Gerardy
+          </span>
+
+        </div>
+
+        <div class="d-none d-md-flex">
+          <v-btn
+              v-for="(item, j) of menuLinks" :key="j"
+              depressed class="menu-links"
+              plain
+              :ripple="false"
+              @click="scrollToSection(item.route)"
+          >
+            {{ item.title }}
+          </v-btn>
+        </div>
+
+        <div class="d-flex d-md-none">
+          <v-btn icon @click="openDrawer">
+            <v-icon>
+              menu
+            </v-icon>
+          </v-btn>
+        </div>
+      </div>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
   name: "bannerHome",
   data: () => ({
-    links:[
+    isActive: null,
+    menuLinks: [
       {
         title: 'Inicio',
-        route: 'mainBanner'
+        route: 'inicio',
       },
       {
-        title: 'Sobre Nosotros',
-        route: 'aboutUs'
+        title: 'Sobre mi',
+        route: 'sobre_mi',
       },
       {
-        title: 'Carreras',
-        route: 'career'
+        title: 'Portafolio',
+        route: 'portafolio',
       },
       {
         title: 'Contacto',
-        route: 'mainBanner'
-      },
+        route: 'contacto',
+      }
     ]
   }),
   methods: {
-
+    openDrawer() {
+      this.isActive = true;
+    },
     scrollToSection(elementName) {
       window.scrollTo({
         top: document.getElementById(elementName).offsetTop,
         behavior: 'smooth'
       });
+      this.isActive = false;
     }
   }
 }
 </script>
 
 <style scoped>
-
-.bannerHome{
-  background-color: #ffffff;
-  z-index: 100;
+.app-bar-style {
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.25) !important;
 }
-
-.bannerHome--title{
-  margin-left: .5rem;
+.app-bar-content {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
 }
-
+.app-logo-title {
+  font-weight: bold;
+  font-size: 18px;
+}
+.app-bar-subhead {
+  font-weight: 300;
+  font-size: 12px;
+  letter-spacing: 0.25em;
+}
+.app-font-size {
+  font-size: 14px;
+}
+/deep/ .v-toolbar__content {
+  padding: 0 !important;
+}
 </style>
